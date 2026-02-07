239a730d574d7f0a7c2f75901a105e667b86605d
norbu3674-afk <norbu3674@gmail.com>
2026-02-07 13:12:16 +0000

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://norbu3674-afk.github.io',
+  base: '/algarve-property',
+});
